adef742e5953daf0600b695b0c16c8f137c6370e
carissamero <carissamero@yahoo.com>
2022-04-10 21:02:59 -0500

    redoing js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,19 +1,16 @@
-function genPassword() {
-    var generateBTN = "#button1";
-    var copyBTN = "#button2"
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?";
-    var passwordLength = 8;
-    var password = "";
-for (var i = 0; i <= passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+    var passwordLength = prompt("How long would you like your password? 6-10 characters?");
+    if(passwordLength < 8 || passwordLength > 10){
+
 }
-    document.getElementById("button1").value = password;
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#button1");
+
+    passwordText.value = password;
 }
-function copyPassword() {
-    var copyText - document.getElementById("button1");
-    copyText.select();
-    document.queryCommandIndeterm("button2");
-}
-generateBTN.addEventlistener ("click", writePassword);
+
+generateBtn.addEventlistener("click", writePassword);
 }
