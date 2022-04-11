29f458f451cc192c39bd087d94213d9ffbca7eb7
carissamero <carissamero@yahoo.com>
2022-04-10 20:37:45 -0500

    deleted js, restarted

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,19 @@
 function genPassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var generateBTN = "#button1";
+    var copyBTN = "#button2"
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?";
     var passwordLength = 8;
     var password = "";
- for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-  }
-        document.getElementById("password").value = password;
+for (var i = 0; i <= passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
 }
-
+    document.getElementById("button1").value = password;
+}
 function copyPassword() {
-  var copyText = document.getElementById("password");
-  copyText.select();
-  document.commandID("copy");  
+    var copyText - document.getElementById("button1");
+    copyText.select();
+    document.queryCommandIndeterm("button2");
+}
+generateBTN.addEventlistener ("click", writePassword);
 }
